6c9ad6a19142156da82e9f4929d3f1cd0e27b45c
gracesuhyun <gracesuhyun@gmail.com>
2022-09-25 00:53:17 -0400

    getting started with GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
   componentDidMount(){
     let name = this.props.route.params.name; // OR ...
     // let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'first test message',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'react native',
+            avatar: 'http://placeimg.com/140/140/any'
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ]
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
 
   render() {
@@ -16,7 +51,15 @@ export default class Chat extends React.Component {
         flex: 1,
         backgroundColor: color
       }}>
-      {/* Rest of the UI */}
+      
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+
       </View>
     );
 };
