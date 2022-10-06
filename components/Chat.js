d120968f614877101069ead72f2c73a195b5c648
gracesuhyun <gracesuhyun@gmail.com>
2022-10-06 02:39:15 -0400

    comment out user.uid for now

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 export default class Chat extends React.Component {
@@ -12,32 +13,30 @@ export default class Chat extends React.Component {
     this.state = {
       messages: [],
       uid: 0,
-      user: {
-        _id: '',
-        name: '',
-        avatar: '',
-      }
     }
 
+    const firebaseConfig = {
+      databaseURL: "chat-app-b0680.firebaseio.com",
+      apiKey: "AIzaSyCySJTysG-rullhFQttw8hFAdoRTRcg1Ec",
+      authDomain: "chat-app-b0680.firebaseapp.com",
+      projectId: "chat-app-b0680",
+      storageBucket: "chat-app-b0680.appspot.com",
+      messagingSenderId: "707100552837",
+      appId: "1:707100552837:web:47a613d1c7253c42ab03ec",
+      measurementId: "G-KLSCV8FY1Z"
+    };
+
     if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: "AIzaSyCPv1Gpaiaw4c_f82j1a0_4yn7Qa892J64",
-        authDomain: "test-1a507.firebaseapp.com",
-        projectId: "test-1a507",
-        storageBucket: "test-1a507.appspot.com",
-        messagingSenderId: "359452519929",
-        appId: "1:359452519929:web:735a5277736c4a062fa128",
-        measurementId: "G-N813M3V1X9"
-      });
+      firebase.initializeApp(firebaseConfig);
     }
     
     this.referenceChatMessages = firebase.firestore().collection('messages');
   }
 
-  onCollectionUpdate = (QuerySnapshot) => {
+  onCollectionUpdate = (querySnapshot) => {
     const messages = [];
    // go through each document
-   QuerySnapshot.forEach((doc) => {
+   querySnapshot.forEach((doc) => {
      // get the QueryDocumentSnapshot's data
      let data = doc.data();
      messages.push({
@@ -52,25 +51,37 @@ export default class Chat extends React.Component {
     });
   };
 
+  addMessages = (message) => {
+
+    this.referenceChatMessages.add({
+        _id: message._id,
+        text: message.text,
+        createdAt: message.createdAt,
+        user: message.user,
+    });
+}
+
   componentDidMount() {
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
     this.referenceChatMessages = firebase.firestore().collection('messages');
-    if (this.referenceChatMessages) {
-        this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate);
-    } else {
-        this.setState({
-            messages: [
-                {
-                    _id: 2,
-                    text: `${name} has entered the chat`,
-                    createdAt: new Date(),
-                    system: true,
-                },
-            ]
-        });
-    }
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+          firebase.auth().signInAnonymously();
+      }
+      this.setState({
+          // uid: user.uid,
+          messages: [],
+      });
+
+      this.referenceChatMessages = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
+
+      this.unsubscribe = this.referenceChatMessages
+          .orderBy('createdAt', 'desc')
+          .onSnapshot(this.onCollectionUpdate);
+    });
   }
 
   componentWillUnmount() {
@@ -92,9 +103,11 @@ export default class Chat extends React.Component {
   }
 
   onSend(messages = []) {
-    this.setState(previousState => ({
+    this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    }), () => {
+      this.addMessages();
+    });
   }
 
   render() {
@@ -108,10 +121,13 @@ export default class Chat extends React.Component {
       
       <GiftedChat
         renderBubble={this.renderBubble.bind(this)}
+        placeholder='Type your message'
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
         user={{
-          _id: 1,
+          _id: this.state.uid,
+          name: this.state.name,
+          avatar: 'ttps://placeimg.com/140/140/people'
         }}
       />
 
